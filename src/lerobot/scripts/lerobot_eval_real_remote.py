3e8312bb5eca750adb585767ad7c194cd8348156
uhroa <uhroa@student.kit.edu>
2025-12-12 14:06:56 +0100

    including remote evaluation to use aloha wiht policy on cluster

---
+++ src/lerobot/scripts/lerobot_eval_real_remote.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Evaluate a policy on an environment by running rollouts and computing metrics.
+
+Usage examples:
+
+You want to evaluate a model from the hub (eg: https://huggingface.co/lerobot/diffusion_pusht)
+for 10 episodes.
+
+```
+lerobot-eval \
+    --policy.path=lerobot/diffusion_pusht \
+    --env.type=pusht \
+    --eval.batch_size=10 \
+    --eval.n_episodes=10 \
+    --policy.use_amp=false \
+    --policy.device=cuda
+```
+
+OR, you want to evaluate a model checkpoint from the LeRobot training script for 10 episodes.
+```
+lerobot-eval \
+    --policy.path=outputs/train/diffusion_pusht/checkpoints/005000/pretrained_model \
+    --env.type=pusht \
+    --eval.batch_size=10 \
+    --eval.n_episodes=10 \
+    --policy.use_amp=false \
+    --policy.device=cuda
+```
+
+Note that in both examples, the repo/folder should contain at least `config.json` and `model.safetensors` files.
+
+You can learn about the CLI options for this script in the `EvalPipelineConfig` in lerobot/configs/eval.py
+"""
+
+import concurrent.futures as cf
+import json
+import logging
+import threading
+import time
+from collections import defaultdict
+from collections.abc import Callable
+from contextlib import nullcontext
+from copy import deepcopy
+from dataclasses import asdict
+from functools import partial
+from pathlib import Path
+from pprint import pformat
+from typing import Any, TypedDict
+
+import einops
+import gymnasium as gym
+import numpy as np
+import torch
+from termcolor import colored
+from torch import Tensor, nn
+from tqdm import trange
+
+from lerobot.configs import parser
+from lerobot.configs.eval import EvalPipelineConfig
+from lerobot.envs.factory import make_env, make_env_pre_post_processors
+from lerobot.envs.utils import (
+    add_envs_task,
+    check_env_attributes_and_types,
+    close_envs,
+    preprocess_observation,
+)
+from lerobot.policies.factory import make_policy, make_pre_post_processors
+from lerobot.policies.pretrained import PreTrainedPolicy
+from lerobot.processor import PolicyAction, PolicyProcessorPipeline
+from lerobot.utils.constants import ACTION, DONE, OBS_STR, REWARD
+from lerobot.utils.import_utils import register_third_party_plugins
+from lerobot.utils.io_utils import write_video
+from lerobot.utils.random_utils import set_seed
+from lerobot.utils.utils import (
+    get_safe_torch_device,
+    init_logging,
+    inside_slurm,
+)
+
+
+
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+policy = None
+preprocessor = None
+postprocessor = None
+device = None
+
+
+@parser.wrap()
+def eval_main(cfg: EvalPipelineConfig):
+    global policy, preprocessor, postprocessor, device
+
+    # Check device is available
+    device = get_safe_torch_device(cfg.policy.device, log=True)
+
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+    set_seed(cfg.seed)
+
+    logging.info(colored("Output dir:", "yellow", attrs=["bold"]) + f" {cfg.output_dir}")
+
+    logging.info("Making policy.")
+
+    policy = make_policy(
+        cfg=cfg.policy,
+        env_cfg=cfg.env,
+        rename_map=cfg.rename_map,
+    )
+
+    policy.eval()
+
+    # The inference device is automatically set to match the detected hardware, overriding any previous device settings from training to ensure compatibility.
+    preprocessor_overrides = {
+        "device_processor": {"device": str(policy.config.device)},
+        "rename_observations_processor": {"rename_map": cfg.rename_map},
+    }
+
+    preprocessor, postprocessor = make_pre_post_processors(
+        policy_cfg=cfg.policy,
+        pretrained_path=cfg.policy.pretrained_path,
+        preprocessor_overrides=preprocessor_overrides,
+    )
+
+    with torch.no_grad(), torch.autocast(device_type=device.type) if cfg.policy.use_amp else nullcontext():
+       if not isinstance(policy, PreTrainedPolicy):
+        raise ValueError(
+            f"Policy of type 'PreTrainedPolicy' is expected, but type '{type(policy)}' was provided."
+        )
+
+    start = time.time()
+    policy.eval()
+    policy.reset()
+
+    # Close all vec envs
+
+    logging.info("End of eval")
+@app.route('/eval', methods=['POST'])
+def eval():
+    global policy, preprocessor, postprocessor, device
+    data = request.get_json()
+    observation = data.get("observation")
+    instruction = data.get("instruction")
+
+
+
+    observation = preprocess_observation(observation)
+
+    observation = preprocessor(observation)
+    with torch.inference_mode():
+        action = policy.select_action(observation)
+    action = postprocessor(action)
+
+    action_transition = {"action": action}
+    action = action_transition["action"]
+
+    # Convert to CPU / numpy.
+    action_numpy: np.ndarray = action.to("cpu").numpy()
+    assert action_numpy.ndim == 2, "Action dimensions should be (batch, action_dim)"
+    return jsonify({"action" : action})
+
+def main():
+    register_third_party_plugins()
+    eval_main()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+    main()
